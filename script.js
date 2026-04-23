4f278f7326c8afa71dde2ae44291ad3f6e9d5632
C2theJ <coleblu84@gmail.com>
2026-04-23 19:04:11 -0400

    changed email icon to copy and paste. add JS to make work.

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.querySelectorAll(".card").forEach((card) => {
     card.classList.toggle("flipped");
   });
 });
+
+document.getElementById("copy-email").addEventListener("click", (e) => {
+  e.stopPropagation();
+
+  const email = "coleblu84@gmail.com";
+
+  navigator.clipboard.writeText(email).then(() => {
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 700);
+  });
+});
